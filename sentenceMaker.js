5019561e36a0bb9bad29e7f0882ede97356501c3
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-03-08 12:17:19 +1300

    created a basic sentence maker on freeCodeCamp, story quality isn't very good

---
+++ sentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "Red";
+let noun = "Dragon";
+let verb = "eating";
+let place = "cave";
+let adjective2 = "hot";
+let noun2 = "peppers";
+
+//First story starts below.
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+//Second story starts below.
+adjective = "little"
+noun = "girl"
+verb = "hungry"
+place = "village"
+adjective2 = "small"
+noun2 = "chocolate"
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory)
